33a6e9e1d33a1ad2e275fd94d2c9e1d4ff691593
AgentIdea Technologies Inc <grantsteinfeld@gmail.com>
2012-12-04 18:13:49 -0500

    basic machine to process dsl added

--- DSL/machine.ps1
+++ DSL/machine.ps1
@@ -23,6 +23,27 @@ Function Get-FrontOfUrl ($commandName) {
  
 Function Add-TreeNode ($treeID,$nodeName,$children,$parent,$template,$tags) {
 
+    $url = Get-FrontOfUrl "CreateNode"
+    $url = $url + "&name=$nodeName"
+    $url = $url + "&nodeID=$nodeName"
+    $url = $url + "&treeID=$treeID"
+    $url = $url + "&parent=$parent"
+    $url = $url + "&children=$children"
+    $url = $url + "&tags=$tags"
+    $url = $url + "&template=$template"
+    $url = $url + "&nodeCollection=astro_test"
+
+    Write-Host -BackgroundColor DarkRed -ForegroundColor Yellow $url
+
+
+    $ret = Call-TreeEndpoint $url
+    $ret.commands | % {
+        Write-Host $_.name
+        $_.parameters | % {
+            Write-Host -BackgroundColor Red $_.name + " " + $_.value
+        }
+
+    }
 
 }
 
@@ -40,37 +61,45 @@ Function Call-TreeEndpoint ($url){
 }
 
 
+Function Confirm-Tree ($treeID){
 
+    Write-Host -BackgroundColor Gray -ForegroundColor DarkMagenta " setting state on tree $stateName "
+
+    $url = Get-FrontOfUrl "Tree2Dag2"
+    $url = $url + "&treeID=$treeID"
+    $url = $url + "&collection=astro_test"
+    $ret = Call-TreeEndpoint $url
+    $ret.commands | % {
+        Write-Host $_.name
+        $_.parameters | % {
+            Write-Host $_.name + " " + $_.value
+        }
+        }
+}
 
 Function Set-TreeState ($treeID,$stateName){
 
     Write-Host -BackgroundColor Gray -ForegroundColor DarkMagenta " setting state on tree $stateName "
 
-
+    $url = Get-FrontOfUrl "SetTreeState"
+    $url = $url + "&treeID=$treeID"
+    $url = $url + "&stateName=$stateName"
+    $url = $url + "&nodeCollection=astro_test"
+    Call-TreeEndpoint $url
 }
 
 
 Function Get-TreeState ($treeID="babU") {
-
-
-
     $url = Get-FrontOfUrl "GetTreeState"
     $url = $url + "&treeID=$treeID"
-
+    $url = $url + "&nodeCollection=astro_test"
     Call-TreeEndpoint $url
-
-   
-
-
-
 }
 
 
 
 # run tests to tree that may not exist ...
 Function Test-TreeState ($treeName) {
-
-
 
     $state = Get-TreeState $treeName
     $state.commands | % {
@@ -85,5 +114,23 @@ Function Test-TreeState ($treeName) {
     }
 }
 
- Test-TreeState "charity"
- Test-TreeState "hope"
+Function Other-Calls {
+
+    Test-TreeState "charity"
+    Test-TreeState "hope"
+    Test-TreeState "babu"
+    Set-TreeState "babu" "accepting_new_nodes"
+
+    Add-TreeNode "babu" "A" "B,C,X" "null" "templateNamedFred" "tags,and,more,blah"
+    Add-TreeNode "babu" "B" "D" "A" "templateNamedFred" "sausages"
+    Add-TreeNode "babu" "C" "D" "A" "templateNamedFred" "jam,toast,beagles"
+    Add-TreeNode "babu" "D" "null" "B,C" "templateNamedFred" "meerkat,emu"
+    Add-TreeNode "babu" "X" "null" "A" "templateNamedFred" "yawl,canoe,wooden"
+
+    Set-TreeState "babu" "ready_for_recalculation"
+
+
+}
+
+
+ Confirm-Tree "babu"
